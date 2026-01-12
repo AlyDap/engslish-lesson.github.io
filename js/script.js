62c4760ad58d5b38205a01b2123ac794a80c2cc0
AlyDap <ferdynan.jr@gmail.com>
2026-01-12 10:53:43 +0700

    new file

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// Inisialisasi DataTables
+$(document).ready(function () {
+  $("#vocabTable").DataTable({
+    paging: true,
+    searching: true,
+    info: true,
+    responsive: true,
+  });
+});
